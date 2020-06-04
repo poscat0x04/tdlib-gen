f0f351be44fff5888cc09c2d13da2d53d5b60458
Poscat <poscat@mail.poscat.moe>
2020-06-04 17:08:09 +0800

    Add a simple test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Codegen
+import qualified Data.Text.IO as T
+import Language.Haskell.Codegen
+import Language.TL.AST
+import Language.TL.Parser
+import Processing
+import Text.Megaparsec
+import Text.Pretty.Simple
+
 main :: IO ()
-main = pure ()
+main = do
+  f <- T.readFile "test/data/td_api.tl"
+  let Right prog = runParser program "" f
+  let (datas, functions) = convProgram prog
+  let adts = fmap (convADT defTyMap) datas
+  let p = fmap sanitizeADT adts
+  pPrint p
